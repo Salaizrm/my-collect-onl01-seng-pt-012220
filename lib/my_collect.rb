2dcff8b522244fd9ed52d7474fdbcabcb6f213a6
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-29 21:47:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(array)
 
-  i = 0
   new_array = []
+  i = new_array
   while i < array.length
-    new_array << yield(array[i])
-    i += 1
+    array << yield(array[i])
+    
   end
 end
 
